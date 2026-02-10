dc28b288e8f98551c2078ec5a983d72fba232627
maskedpiss <joshkeckedits@proton.me>
2026-02-10 11:52:57 -0600

    drew ground

--- src/objs/gameworld.lua
+++ src/objs/gameworld.lua
@@ -5,7 +5,12 @@ function GameWorld:load()
       color = {0, 0, 1}
   }
   
-  self.Ground = {}
+  self.Ground = {
+      x = Globals.Screen.x,
+      y = Globals.Screen.height - 100,
+      width = Globals.Screen.width,
+      height = 100
+  }
 end
 
 
@@ -16,6 +21,9 @@ end
 
 function GameWorld:draw()
   love.graphics.setBackgroundColor(self.BG.color)
+  
+  love.graphics.setColor(0.4, 0.67, 0.49)
+  love.graphics.rectangle("fill", self.Ground.x, self.Ground.y, self.Ground.width, self.Ground.height)
 end
 
 return GameWorld
