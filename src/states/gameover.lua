aa14e21b8852dbb61e839a23099fad802318b3fa
maskedpiss <joshkeckedits@proton.me>
2026-02-11 13:03:35 -0600

    drew gameover message

--- src/states/gameover.lua
+++ src/states/gameover.lua
@@ -1,7 +1,13 @@
 local GameOver = {}
 
+local titleFont = love.graphics.newFont(128)
+
 function GameOver.onEnter()
-  
+  GameOver.Message = {
+      text = "Game Over!",
+      x = Globals.Screen.x,
+      y = 50
+  }
 end
 
 
@@ -11,7 +17,9 @@ end
 
 
 function GameOver.draw()
-  
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.setFont(titleFont)
+  love.graphics.printf(GameOver.Message.text, GameOver.Message.x, GameOver.Message.y, Globals.Screen.width, "center")
 end
 
 
