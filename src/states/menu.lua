00cfbad80a5e604440836d4b85ce0a51871996d0
maskedpiss <joshkeckedits@proton.me>
2026-02-10 22:06:26 -0600

    drew play button

--- src/states/menu.lua
+++ src/states/menu.lua
@@ -2,12 +2,18 @@ local Menu = {}
 
 local titleFont = love.graphics.newFont(128)
 
+local Buttons = nil
+local playButton = nil
+
 function Menu.onEnter()
   Menu.Title = {
       text = "Jetpack Joyride",
       x = Globals.Screen.x,
       y = 50
   }
+  
+  Buttons = require("src/objs/button")
+  playButton = Buttons.new("Play", Globals.Screen.width / 2, Globals.Screen.height / 2)
 end
 
 
@@ -20,6 +26,8 @@ function Menu.draw()
   love.graphics.setColor(1, 1, 1)
   love.graphics.setFont(titleFont)
   love.graphics.printf(Menu.Title.text, Menu.Title.x, Menu.Title.y, Globals.Screen.width, "center")
+  
+  playButton:draw()
 end
 
 
