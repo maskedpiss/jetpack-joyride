74e84614f0b331890a159cf9f5b57d54489f5855
maskedpiss <joshkeckedits@proton.me>
2026-02-11 13:25:43 -0600

    drew gameover buttons

--- src/states/gameover.lua
+++ src/states/gameover.lua
@@ -2,12 +2,20 @@ local GameOver = {}
 
 local titleFont = love.graphics.newFont(128)
 
+local Buttons = nil
+local retryButton = nil
+local exitButton = nil
+
 function GameOver.onEnter()
   GameOver.Message = {
       text = "Game Over!",
       x = Globals.Screen.x,
       y = 50
   }
+  
+  Buttons = require("src/objs/button")
+  retryButton = Buttons.new("Retry", Globals.Screen.width / 2, Globals.Screen.height / 2)
+  exitButton = Buttons.new("Exit", Globals.Screen.width / 2, (Globals.Screen.height / 2) + 100)
 end
 
 
@@ -20,6 +28,9 @@ function GameOver.draw()
   love.graphics.setColor(1, 1, 1)
   love.graphics.setFont(titleFont)
   love.graphics.printf(GameOver.Message.text, GameOver.Message.x, GameOver.Message.y, Globals.Screen.width, "center")
+  
+  retryButton:draw()
+  exitButton:draw()
 end
 
 
