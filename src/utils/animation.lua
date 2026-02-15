65ad07e525ae635eabced887c318a22d7e977cbc
maskedpiss <joshkeckedits@proton.me>
2026-02-15 11:47:27 -0600

    wrote function for parsing sprite sheets into quads

--- src/utils/animation.lua
+++ src/utils/animation.lua
@@ -1,7 +1,16 @@
 local Animation = {}
 
-function parseSpriteSheet()
+function parseSpriteSheet(texture, frameWidth, frameHeight)
+  local sheetWidth, sheetHeight = texture:getDimensions()
+  local quads = {}
+  local counter = 1
 
+  for y = 0, sheetHeight - 1, frameHeight do
+	for x = 0, sheetWidth - 1, frameWidth do
+		quads[counter] = love.graphics.newQuad(x, y, frameWidth, frameHeight, sheetWidth, sheetHeight)
+		counter = counter + 1
+	end
+  end
 end
 
 return Animation
