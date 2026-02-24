0da87800f59e2e8c4a88aa685f2e776a1dbe5c11
maskedpiss <joshkeckedits@proton.me>
2026-02-24 14:28:48 -0600

    set up zapper manager functions

---
+++ src/utils/zappermanager.lua
@@ -0,0 +1,22 @@
+local Zapper = require("src.objs.obstacles.zapper")
+local ZapperManager = {}
+
+ZapperManager.pool = {}
+ZapperManager.poolSize = 5
+ZapperManager.spawnDistance = 400
+
+function ZapperManager:load()
+
+end
+
+
+function ZapperManager:update(dt)
+
+end
+
+
+function ZapperManager:draw()
+
+end
+
+return ZapperManager
