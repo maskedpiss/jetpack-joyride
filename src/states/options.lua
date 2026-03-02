623ff843d5125eb3c5fdd35bf89034f1012809fe
maskedpiss <joshkeckedits@proton.me>
2026-03-02 17:45:48 -0600

    drew sky bg in options menu

--- src/states/options.lua
+++ src/states/options.lua
@@ -4,7 +4,15 @@ local vsyncToggle = nil
 local backButton = nil
 
 function Options.onEnter()
-  Options.BG = {}
+  Options.BG = {
+  	sprite = Globals.Graphics.Sprites.BG1,
+  	x = Globals.Screen.x,
+  	y = Globals.Screen.y,
+  	width = Globals.Graphics.Sprites.BG1:getWidth(),
+  	height = Globals.Graphics.Sprites.BG1:getHeight(),
+  	speed = 50
+  }
+  
   Options.Title = {}
 end
 
@@ -15,7 +23,8 @@ end
 
 
 function Options.draw()
-
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.draw(Options.BG.sprite, Options.BG.x, Options.BG.y)
 end
 
 
