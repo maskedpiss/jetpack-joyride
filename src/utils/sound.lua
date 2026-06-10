1f36ac82c3cbdac1b2e754e5b4a3dc266c4470f2
maskedpiss <joshkeckedits@proton.me>
2026-06-10 16:11:22 -0500

    made sfx for lasers

--- src/utils/sound.lua
+++ src/utils/sound.lua
@@ -8,7 +8,10 @@ function Sound:loadSFX()
 		Switch = love.audio.newSource("res/sound/ui-switch-pull-switch-lamp-01.wav", "static"),
 		Footsteps = love.audio.newSource("res/sound/footsteps.wav", "static"),
 		Shooting = love.audio.newSource("res/sound/Shooting.wav", "static"),
-		BulletImpact = love.audio.newSource("res/sound/hit-impact.ogg", "static")
+		BulletImpact = love.audio.newSource("res/sound/hit-impact.ogg", "static"),
+		LaserEngage = love.audio.newSource("res/sound/laser-engage.wav", "static"),
+		LaserDisengage = love.audio.newSource("res/sound/laser-disengage.wav", "static"),
+		LaserHum = love.audio.newSource("res/sound/laser-hum.wav", "static")
 	}
 
 	self.SFX.Footsteps:setLooping(true)
